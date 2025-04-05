59a9d881dbe5c636209e1812ac0cb654614497c9
ndy <dynam99@gmail.com>
2025-04-06 00:54:17 +0900

    init

---
+++ src/main/java/com/example/discordbot/DiscordBotApplication.java
@@ -0,0 +1,29 @@
+package com.example.discordbot;
+
+import com.example.discordbot.config.DiscordConfig;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.security.auth.login.LoginException;
+
+@SpringBootApplication
+public class DiscordBotApplication {
+
+    public static void main(String[] args) throws LoginException {
+        ApplicationContext context = SpringApplication.run(DiscordBotApplication.class, args);
+        DiscordConfig discordConfig = context.getBean(DiscordConfig.class);
+        String discordBotToken = discordConfig.getDiscordBotToken();
+
+        JDA jda = JDABuilder.createDefault(discordBotToken)
+                .setActivity(Activity.playing("메시지 기다리는 중!"))
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .build();
+
+    }
+
+}
---
+++ src/main/java/com/example/discordbot/config/DiscordConfig.java
@@ -0,0 +1,13 @@
+package com.example.discordbot.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class DiscordConfig {
+    @Value("${discord.bot.token}")
+    private String discordBotToken;
+
+}
